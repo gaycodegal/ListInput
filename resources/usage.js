ebe101794e377f6348ddc2a2bd5bc0e14d0eafc9
stephoro <stephenjoro@gmail.com>
2017-01-21 16:21:50 -0500

    Minor adjustment

--- resources/usage.js
+++ resources/usage.js
@@ -34,8 +34,3 @@ myinputsetvals.setData(["Banana","Apple","Pear"]);
 lib.append(multiSample, ListInput.make(undefined, "long things",["ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz","Something Short","Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."]));
 
 lib.append(multiSample, lib.input("submit"));
-
-
-lib.getObject("input").each(function(o){
-  o.style.display = null;
-});
